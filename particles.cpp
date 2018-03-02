13a74d02c1ea91da3369c1c2ef485fc1185eedc4
Max Zuev <maxnzv@gmail.com>
2018-03-02 21:10:54 +0000

    First move

--- particles.cpp
+++ particles.cpp
@@ -6,19 +6,20 @@
 #include <xcb/xcb.h>
 
 // Particles amount
-const int N = 16;
+const int N = 80;
 // Gravity constant
-const double G = 10.0;
-const double minX = 10.0;
-const double minY = 10.0;
+const double G = 0.0001;
+const double minX = 0.0;
+const double minY = 0.0;
 const double maxX = 700.0;
 const double maxY = 700.0;
 // Minimal distance to reverce the force between particles
-const double minD2 = 5.0 * 5.0;
+const double minD2 = 2.0 * 2.0;
 // Border width that reverses velocity
 const int border = 10;
 // Standart delay between display iterations
-timespec delay = { 1, 0 };
+timespec delay = { 0, 2000000 };
+//timespec delay = { 1, 0 };
 
 class TParticle {
     // Mass
@@ -50,22 +51,28 @@ class TParticle {
     double GetY (void) { return y; }
     double GetM (void) { return m; }
 
+    // Save coordinates to future drawing
+    void Draw (void) {
+      xcbpoint->x = (int)x;
+      xcbpoint->y = (int)y;
+    }
+
     // Move the particle according on force, time and initial speed
     // Use clock_gettime with CLOCK_MONOTONIC
     void Move ( long t ) {
       // Some optimization
       double tm = t / m;
       double mt22 = tm*t / 2.0;
+
+      // Border protection
+      if ( x < minX+border or x > maxX-border ) vx = -vx*0.9;
+      if ( y < minY+border or y > maxY-border ) vy = -vy*0.9;
+
       x += vx * t + fx * mt22;
       y += vy * t + fy * mt22;
       vx += fx * tm;
       vy += fy * tm;
       fx = fy = 0.0;
-      // Border protection
-      if ( x < minX or x > maxX ) vx = -vx;
-      if ( y < minY or y > maxY ) vy = -vy;
-      xcbpoint->x = (int)x;
-      xcbpoint->y = (int)y;
     }
 
     // Calculate force between two particles and add it to both
@@ -75,13 +82,13 @@ class TParticle {
       double m1 = neighbor->GetM();
       double d2 = dx*dx + dy*dy; // distance ^ 2 (Pifagor)
       double f = G * m*m1/d2;
-      if (d2 < minD2) { f = -f; }
+      if (d2 < minD2) { f = -f/100; }
       double dfx = f * dx / dy;
       double dfy = f * dy / dx;
-      fx -= dfx;
-      fy -= dfy;
-      neighbor->fx += dfx;
-      neighbor->fy += dfy;
+      fx += dfx;
+      fy += dfy;
+      neighbor->fx -= dfx;
+      neighbor->fy -= dfy;
     }
 };
 
@@ -93,28 +100,50 @@ class TPArray {
       xpoints = xp;
       container = new TParticle[N];
       for (int i=0; i<N; i++) {
-        container[i].Init (maxX*i/N, maxY*i/N, (double)i, xpoints + i);
+        container[i].Init ((maxX-border)*i/N + sqrt(i)+border, (maxY-border)*i/N+border, 0.5, xpoints + i);
       }
     }
+
+    // Calculate forces among all particles
     void Calculate (void) {
       for (int i=0; i<N-1; i++) {
-        for (int j=i; j<N; j++) {
+        for (int j=i+1; j<N; j++) {
           container[i].CalcForce (container + j);
-	}
+        }
       }
     }
+
+    // Move every particle according applied forces
     void Move (long t) {
-      for (int i=0; i<N-1; i++) {
+      for (int i=0; i<N; i++) {
         container[i].Move (t);
       }
     }
+
+    // Save every particle position
+    void Draw (void) {
+      for (int i=0; i<N; i++) {
+        container[i].Draw ();
+      }
+    }
+    ~TPArray (){
+      delete container;
+    }
 };
+
+void CalcAndMove (TPArray *ppa) {
+  //while (1) {
+    ppa->Calculate();
+    ppa->Move(1);
+  //}
+}
 
 int main () {
   xcb_connection_t    *connection;
   xcb_screen_t        *screen;
   xcb_drawable_t       win;
   xcb_gcontext_t       foreground;
+  xcb_gcontext_t       background;
   xcb_generic_event_t *event;
   uint32_t             mask = 0;
   uint32_t             values[2];
@@ -124,7 +153,7 @@ int main () {
   TPArray tpa(points);
 
   for (int i=0; i<N; i++) {
-    std::cout<<points[i].x<<std::endl;
+    std::cout<<points[i].x<<"-"<<points[i].y<<std::endl;
   }
 
   /* Open the connection to the X server */
@@ -142,6 +171,12 @@ int main () {
   values[1] = 0;
   xcb_create_gc (connection, foreground, win, mask, values);
 
+  background = xcb_generate_id (connection);
+  mask = XCB_GC_FOREGROUND | XCB_GC_GRAPHICS_EXPOSURES;
+  values[0] = screen->black_pixel;
+  values[1] = 0;
+  xcb_create_gc (connection, background, win, mask, values);
+
   /* Ask for our window's Id */
   win = xcb_generate_id(connection);
 
@@ -154,7 +189,7 @@ int main () {
                      win,                           /* window Id           */
                      screen->root,                  /* parent window       */
                      0, 0,                          /* x, y                */
-                     int(maxX)+border, int(maxY)+border,                      /* width, height       */
+                     int(maxX), int(maxY),                      /* width, height       */
                      10,                            /* border_width        */
                      XCB_WINDOW_CLASS_INPUT_OUTPUT, /* class               */
                      screen->root_visual,           /* visual              */
@@ -170,15 +205,36 @@ int main () {
   /* Map the window on the screen */
   xcb_map_window (connection, win);
 
-
   /* We flush the request */
   xcb_flush (connection);
 
+  int n = 0;
+
   while (1) {
     if ((event = xcb_poll_for_event (connection))== NULL) {
-        clock_nanosleep (CLOCK_MONOTONIC, 0, &delay, &delay);
-	      std::cout<<"tick"<<std::endl;
-        continue;
+      clock_nanosleep (CLOCK_MONOTONIC, 0, &delay, &delay);
+      CalcAndMove(&tpa);
+
+      //tpa.Draw();
+      //for (int i=0; i<N; i++) {
+        //std::cout<<points[i].x<<std::endl;
+      //}
+
+      if (n % 50 == 0) {
+        xcb_poly_point (connection, XCB_COORD_MODE_ORIGIN, win, background, N, points);
+        tpa.Draw();
+        xcb_poly_point (connection, XCB_COORD_MODE_ORIGIN, win, foreground, N, points);
+        xcb_flush (connection);
+      }
+      if (n == 500) {
+        std::cout<<"tick"<<std::endl;
+        n = 0;
+        for (int i=0; i<N; i++) {
+          std::cout<<points[i].x<<"-"<<points[i].y<<std::endl;
+        }
+      }
+      else {n++;}
+      continue;
     }
     switch (event->response_type & ~0x80) {
       case XCB_EXPOSE: {
